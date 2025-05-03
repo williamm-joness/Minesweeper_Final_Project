0d8d25d986f414328effe1acebf5824d6dda1c33
William Jones <benny@Williams-Macbook-Pro.local>
2025-05-03 17:47:49 -0400

    play function outlined

---
+++ Minesweeper.py
@@ -0,0 +1,10 @@
+def play()
+    # 1) create the board, plant the bombs randomly
+    # 2) display the board, ask user where they want to dig
+    # 3) if location is a bomb, end game/ show game over
+    # 4) if location is safe, reveal number underneath
+    # 5) repeat steps 2-4 until either
+    #   a) there are no more squares that aren't bombs
+    #   b) user hits a bomb
+    # 6) if a), display you win
+
